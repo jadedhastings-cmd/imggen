d34d7d10caf1d0e4d32b280b1ce64d906429d934
JD Hastings <jaded.hastings@gmail.com>
2026-03-13 04:34:42 -0700

    Remove unused scripts, keep laserprep, shapes, and shapefuncJSON

--- circle.py
+++
@@ -1,9 +0,0 @@
-import svgwrite
-import cairosvg
-
-# SVG
-dwg = svgwrite.Drawing("circle.svg", size=(200, 200))
-dwg.add(dwg.ellipse(center=(100, 100), r=(50, 50), stroke="black", stroke_width=2, fill="none"))
-dwg.save()
-
-cairosvg.svg2png(url="circle.svg", write_to="circle_from_svg.png", background_color="white")
